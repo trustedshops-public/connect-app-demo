11be9f83a79a723bb848431abee415def83dcd9c
mikins1991 <mikins19911@gmail.com>
2023-03-15 16:12:58 +0300

    initial tests

---
+++ src/test-exaples/Counter.tsx
@@ -0,0 +1,15 @@
+import { h } from 'preact'
+import { FC } from 'preact/compat'
+import { useState } from 'preact/hooks'
+
+export const Counter: FC<{ initialCount: number }> = ({ initialCount }) => {
+  const [count, setCount] = useState(initialCount)
+  const increment = () => setCount(count + 1)
+
+  return (
+    <div>
+      Current value: {count}
+      <button onClick={increment}>Increment</button>
+    </div>
+  )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
-import path, { join } from 'path'
+import { join } from 'path'
 import EnvironmentPlugin from 'vite-plugin-environment'
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -14,13 +14,15 @@ export default defineConfig(() => {
     server: {
       port: 3000,
     },
-    build: {
-      outDir: 'build',
-      lib: {
-        entry: path.resolve(__dirname, 'src/main.tsx'),
-        name: 'Connector',
-        fileName: format => `connector.${format}.js`,
+    test: {
+      environment: 'happy-dom',
+      setupFiles: ['./__test__/test-setup.ts'],
+      includeSource: ['src/**/*.{ts,tsx}'],
+      coverage: {
+        reporter: ['text-summary', 'text'],
       },
+      mockReset: true,
+      restoreMocks: true,
     },
   }
 })
