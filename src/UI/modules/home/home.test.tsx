3ddd1059058d2779a4d11bd3e5233abf11a63f4a
mikins1991 <mikins19911@gmail.com>
2023-03-23 17:40:25 +0300

    experiment: fix home page test error

--- src/UI/modules/home/home.test.tsx
+++ src/UI/modules/home/home.test.tsx
@@ -1,7 +1,8 @@
 import { h } from 'preact'
 import { afterEach, describe, expect, test } from 'vitest'
-import { cleanup, fireEvent, render, screen } from '@testing-library/preact'
+import { cleanup, fireEvent, render, renderHook, screen } from '@testing-library/preact'
 import { HomePageModule } from '.'
+import { useMockDataBaseForBaseLayer } from '@/database-container/useMockDataBaseForBaseLayer'
 
 afterEach(() => {
   cleanup()
@@ -10,18 +11,19 @@ setTimeout(() => {
   return 30000
 })
 describe('Render home page', () => {
-  test('click Open button - should display Connector by ID "shadowRoot"', async () => {
-      render(<HomePageModule />)
-      fireEvent.click(screen.getByTestId('button_open_connector'))
-      await screen.findByTestId('shadowRoot')
-      expect(screen.queryByTestId('shadowRoot')).toBeInTheDocument()
+  test('double click open/close - connector should show/hide', async () => {
+    renderHook(() => useMockDataBaseForBaseLayer())
+    render(<HomePageModule />)
+    // Open connector
+    fireEvent.click(screen.getByTestId('button_open_connector'))
+    await screen.findByTestId('shadowRoot')
+    expect(screen.queryByTestId('shadowRoot')).toBeInTheDocument()
 
-      fireEvent.click(screen.getByTestId('button_open_connector'))
-      expect(screen.queryByTestId('shadowRoot')).not.toBeInTheDocument()
-
-      //fireEvent.click(screen.getByTestId('button_open_connector'))
-      //await screen.findByTestId('shadowRoot')
-      //expect(screen.queryByTestId('shadowRoot')).toBeInTheDocument()
+    //Close connector
+    fireEvent.click(screen.getByTestId('button_open_connector'))
+    await screen.findByText('Open connector')
+    expect(screen.queryByTestId('shadowRoot')).not.toBeInTheDocument()
+    cleanup()
   })
   test('click Open button - button text: Close connector', async () => {
     render(<HomePageModule />)
