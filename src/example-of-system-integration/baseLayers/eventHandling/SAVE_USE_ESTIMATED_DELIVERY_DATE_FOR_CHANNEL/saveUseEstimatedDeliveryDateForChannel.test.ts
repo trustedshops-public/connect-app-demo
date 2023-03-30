24bb18e43d2f2fa89d4fc5c0ea56941e4141d7ff
mikins1991 <mikins19911@gmail.com>
2023-03-30 17:21:35 +0300

    test: saveUseEstimatedDeliveryDateForChannel

--- src/example-of-system-integration/baseLayers/eventHandling/SAVE_USE_ESTIMATED_DELIVERY_DATE_FOR_CHANNEL/saveUseEstimatedDeliveryDateForChannel.test.ts
+++ src/example-of-system-integration/baseLayers/eventHandling/SAVE_USE_ESTIMATED_DELIVERY_DATE_FOR_CHANNEL/saveUseEstimatedDeliveryDateForChannel.test.ts
@@ -1,6 +1,6 @@
 import { beforeEach, describe, expect, test } from 'vitest'
 import { EVENTS, registerEvents } from '@/example-of-system-integration/eventsLib'
-import { saveUseEventsByOrderStatusForChannel } from '@/example-of-system-integration/baseLayers/eventHandling'
+import { saveUseEstimatedDeliveryDateForChannel } from '@/example-of-system-integration/baseLayers/eventHandling'
 import { renderHook } from '@testing-library/preact'
 import { db, useMockDataBaseForBaseLayer } from '@/database-container/useMockDataBaseForBaseLayer'
 import { Estimatepayload } from '@/database-container/data-config'
@@ -8,8 +8,8 @@ import { Estimatepayload } from '@/database-container/data-config'
 beforeEach(() => {
   renderHook(useMockDataBaseForBaseLayer)
 })
-describe('Save: use events by order status for channel', () => {
-  test('activate: SET_USE_EVENTS_BY_ORDER_STATUS_FOR_CHANNEL payload should match with testPayloadByOrderStatus', () => {
+describe('Save: use estimated delivery date for channel', () => {
+  test('activate: SET_USE_ESTIMATED_DELIVERY_DATE_FOR_CHANNEL payload should match with testPayloadByOrderStatus', () => {
     let returnedPayloadByOrderStatus = {} as Estimatepayload
 
     const testPayloadByOrderStatus: Estimatepayload = {
@@ -19,24 +19,24 @@ describe('Save: use events by order status for channel', () => {
     }
 
     const unsubscribe = registerEvents({
-      [EVENTS.SET_USE_EVENTS_BY_ORDER_STATUS_FOR_CHANNEL]: function (event: {
+      [EVENTS.SET_USE_ESTIMATED_DELIVERY_DATE_FOR_CHANNEL]: function (event: {
         payload: Estimatepayload
       }) {
         returnedPayloadByOrderStatus = event.payload
       },
     })
 
-    saveUseEventsByOrderStatusForChannel({ payload: testPayloadByOrderStatus })
+    saveUseEstimatedDeliveryDateForChannel({ payload: testPayloadByOrderStatus })
 
     expect(returnedPayloadByOrderStatus.eTrustedChannelRef).toBe(
       testPayloadByOrderStatus.eTrustedChannelRef
     )
     expect(returnedPayloadByOrderStatus.active).toBe(true)
-    expect(db.data?.useEventsByOrderStatusForChannel.length).toBe(1)
-    expect(db.data?.useEventsByOrderStatusForChannel[0].active).toBe(true)
+    expect(db.data?.useEstimatedDeliveryDateForChannel.length).toBe(1)
+    expect(db.data?.useEstimatedDeliveryDateForChannel[0].active).toBe(true)
     unsubscribe()
   })
-  test('deactivate: SET_USE_EVENTS_BY_ORDER_STATUS_FOR_CHANNEL payload should match with testPayloadByOrderStatus', () => {
+  test('deactivate: SET_USE_ESTIMATED_DELIVERY_DATE_FOR_CHANNEL payload should match with testPayloadByOrderStatus', () => {
     let returnedPayloadByOrderStatus = {} as Estimatepayload
 
     const testPayloadByOrderStatus: Estimatepayload = {
@@ -46,21 +46,21 @@ describe('Save: use events by order status for channel', () => {
     }
 
     const unsubscribe = registerEvents({
-      [EVENTS.SET_USE_EVENTS_BY_ORDER_STATUS_FOR_CHANNEL]: function (event: {
+      [EVENTS.SET_USE_ESTIMATED_DELIVERY_DATE_FOR_CHANNEL]: function (event: {
         payload: Estimatepayload
       }) {
         returnedPayloadByOrderStatus = event.payload
       },
     })
 
-    saveUseEventsByOrderStatusForChannel({ payload: testPayloadByOrderStatus })
+    saveUseEstimatedDeliveryDateForChannel({ payload: testPayloadByOrderStatus })
 
     expect(returnedPayloadByOrderStatus.eTrustedChannelRef).toBe(
       testPayloadByOrderStatus.eTrustedChannelRef
     )
     expect(returnedPayloadByOrderStatus.active).toBe(false)
-    expect(db.data?.useEventsByOrderStatusForChannel.length).toBe(1)
-    expect(db.data?.useEventsByOrderStatusForChannel[0].active).toBe(false)
+    expect(db.data?.useEstimatedDeliveryDateForChannel.length).toBe(1)
+    expect(db.data?.useEstimatedDeliveryDateForChannel[0].active).toBe(false)
     unsubscribe()
   })
 })
