a15b0cdecf2a89a48e504350824feb137ec3ed12
Said Tahali <said.tahali@trustedshops.de>
2023-04-03 07:16:52 +0200

    adding widget location test

--- src/example-of-system-integration/baseLayers/eventHandling/GET_LOCALE/getLocale.test.ts
+++ src/example-of-system-integration/baseLayers/eventHandling/GET_LOCALE/getLocale.test.ts
@@ -1,24 +1,20 @@
 import { describe, expect, test } from 'vitest'
-import { getLocationsForWidget } from '@/example-of-system-integration/baseLayers/eventHandling'
+import { getLocale } from '@/example-of-system-integration/baseLayers/eventHandling'
 import { renderHook } from '@testing-library/preact'
 import { useMockDataBaseForBaseLayer } from '@/database-container/useMockDataBaseForBaseLayer'
 import { EVENTS, registerEvents } from '@/example-of-system-integration/eventsLib'
-import { IWidgetLocation } from '@/example-of-system-integration/baseLayers/types'
 
 describe('Get local',()=>{
   test('Get local',()=>{
     renderHook(useMockDataBaseForBaseLayer)
-    let widgetLocation: Array<IWidgetLocation> = [] as Array<IWidgetLocation>
+    let location: string = ''
     const unsubscribe = registerEvents({
-      [EVENTS.SET_LOCATION_FOR_WIDGET]: function (event: { payload: Array<IWidgetLocation> }) {
-        widgetLocation = event.payload
+      [EVENTS.SET_LOCALE]: function (event: { payload: string }) {
+        location = event.payload
       },
     })
-    getLocationsForWidget()
-    expect(widgetLocation.length).toBeGreaterThan(0)
-    expect(widgetLocation[0]).toHaveProperty('id')
-    expect(widgetLocation[0]).toHaveProperty('name')
-    console.table(widgetLocation)
+    getLocale()
+    expect(location).toBe('en-GB')
     unsubscribe()
   })
 })
