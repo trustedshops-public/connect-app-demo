5a93092c46e6ad0f47ea6704d3d1ac200ee94399
Mhera17 <mher.ananyan@aterise.com>
2023-04-26 17:04:40 +0400

    remove spaces before dispatch

--- src/example-of-system-integration/baseLayers/eventHandling/GET_CREDENTIALS_PROVIDED/getCredentialsProvided.ts
+++ src/example-of-system-integration/baseLayers/eventHandling/GET_CREDENTIALS_PROVIDED/getCredentialsProvided.ts
@@ -2,12 +2,20 @@ import { api } from '@/database-container/api/api'
 import { dispatchAction, EVENTS } from '@/example-of-system-integration/eventsLib'
 import { BaseLayerLogger } from '@/utils/BaseLayerLogger'
 
+const removeSpaces = (cred: string) => {
+  return cred.replace(/\s+/g, '')
+}
+
 export function getCredentialsProvided() {
   const credentialsFromApi = api.getCredentials()
-  BaseLayerLogger('Demo: GET_CREDENTIALS_PROVIDED. Answer:', credentialsFromApi)
+  const trimmedCredentials = {
+    clientId: removeSpaces(credentialsFromApi?.clientId || ''),
+    clientSecret: removeSpaces(credentialsFromApi?.clientSecret || ''),
+  }
+  BaseLayerLogger('Demo: GET_CREDENTIALS_PROVIDED. Answer:', trimmedCredentials)
 
   dispatchAction({
     action: EVENTS.SET_CREDENTIALS_PROVIDED,
-    payload: credentialsFromApi,
+    payload: trimmedCredentials,
   })
 }
